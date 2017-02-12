31e015d420bdbfdde4adecc95e0bdcb7e07ac7d7
huhuang03 <huhuang03@gmail.com>
2017-02-12 11:30:12 +0800

    auto commit

--- chapter2/2_1.c
+++ chapter2/2_1.c
@@ -2,8 +2,26 @@
 void printBits(char c);
 
 int main() {
-  char c = 10;
-  printBits((unsigned char)~0 >> 1);
+  char c;
+  c = -(char)((unsigned char)~0 >> 1) - 1;
+  printf("Minimum Singed Char %d\n", c);
+  printf("Maxinum Singed Char %d\n", (char)((unsigned char) ~0 >> 1));
+
+  printf("Minimum Singed Short %d\n", -(short)((unsigned short)~0 >> 1) - 1);
+  printf("Maxinum Singed Short %d\n", (short)((unsigned short) ~0 >> 1));
+
+  printf("Minimum Singed Int %d\n", -(int)((unsigned int)~0 >> 1) - 1);
+  printf("Maxinum Singed Int %d\n", (int)((unsigned int) ~0 >> 1));
+
+  printf("Minimum Singed Long %ld\n", (long)((unsigned long)~0 >> 1) - 1);
+  printf("Maxinum Singed Long %ld\n", (long)((unsigned long) ~0 >> 1));
+
+  printf("Maxinum Unsigned  Char: %d\n", (unsigned char)~0);
+  printf("Maxinum Unsigned  Short: %d\n", (unsigned short)~0);
+  printf("Maxinum Unsigned  Int: %u\n", (unsigned int)~0);
+  printf("Maxinum Unsigned  Long: %lu\n", (unsigned long)~0);
+  
+  printBits(c);
 }
 
 void printBits(char c) {
