1414e0070860cd63e572b8d9a0803fe11fb5e68c
huhuang03 <huhuang03@gmail.com>
2017-03-03 11:30:01 +0800

    auto commit

---
+++ chapter4/4_6.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "getch.h"
+#include "floats.h"
+#include "gettop.h"
+#include "math.h"
+
+#define MAXOP 100
+
+void mathfnc(char []);
+
+int main() {
+  int type;
+  double op2;
+  char s[MAXOP];
+  double variable[26];
+
+  while ((type = gettop(s)) != EOF) {
+    //printf("aaa");
+    //printf("type %d" , type);
+    switch (type) {
+    case NUMBER:
+      push(atof(s));
+      break;
+    case NAME:
+      mathfnc(s);
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() + pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 = 0)
+    	push(pop() / op2);
+      else
+    	printf("error: zero divisor\n");
+      break;
+    case '%':
+      op2 = pop();
+      push(pop() % op2);
+      break;
+    case '\n':
+      printf("%t%.8g\n", pop());
+      break;
+    case '=':
+      pop;
+      if (var >= 'A' && var <= 'Z')
+	variable[var-'A'] = pop();
+      else
+	printf("error: novariablename\n");
+      break;
+    default:
+      if (type >= 'A' && type <= 'Z')
+	push(variable[type-'A']);
+      else if (type='v')
+	push(v);
+      else
+	printf("error unknow command %s\n", s);
+      break;
+    }
+    var = type;
+  }
+  return 0;
+}
+
+void mathfunc(char s[]) {
+  double op2;
+  if (strcmp(s, "sin") == 0) {
+    push(sin(pop()));
+  } else if (strcmp(s, "cos") == 0) {
+    push(cos(pop()));
+  } else if (strcmp(s, "exp") == 0) {
+    push(exp(pop()));
+  } else if (strcmp(s, "pow") == 0) {
+    op2 = pop();
+    push(power(pop(), op2));
+  } else {
+    printf("error: %s is not supporte\n", s);
+  }
+}
